9c0f8dba1f7d6e5976c6ddc8874b44b863ff8d86
Kyle <kyle@weld.app>
2022-07-06 23:31:23 +0200

    IronmanJS

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # IronmanJS
 Bringing Ironman mode to JavaScript
+
+
+Do you wish JavaScript was that little bit more challenging? Do you want to ensure you have absolutely no dependencies? Then IronmanJS is for you.
+
+
+This simple (yet effective) package prevents any other packages from being installed in your codebase. It does so by scanning `package.json` for any (and all) dependencies, if it detects any additional dependencies it throws an error.
+
+# Usage
+Using IronmanJS is simple and easy, simply include this single line at the beginning of any file that is used in your code (we recommend your app's entrypoint):
+```javascript
+require('ironmanjs');
+``` 
---
+++ index.js
@@ -0,0 +1,36 @@
+// License at bottom of file, because who wants to start by reading a license?
+const fs = require('fs');
+
+const whitelist = process.env.IRONMANJS_WHITELIST?.split(',') || [];
+whitelist.push('ironmanjs');
+const whitelistMap = whitelist.reduce((prev, cur) => { prev[cur] = true; return prev; }, {});
+
+fs.readFile('package.json', (err, data) => {
+    if (err) {
+        if (!err.code === 'ENOENT') throw new Error('IronmanJS - Cannot read package.json');
+        return;
+    }
+
+    const dependencies = Object.keys(JSON.parse(data.toString())['dependencies']);
+    const extraDependencies = dependencies.filter(dep => !whitelistMap[dep])
+
+    if (extraDependencies.length !== 0) {
+        console.log(
+            'Uninstall these packages to get the ironman JS experience: "' +
+            extraDependencies.join('", "') + '"'
+        );
+        
+
+        throw new Error('IronmanJS - Too Many Packages');
+    }     
+});
+
+
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <miningape@gmail.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.  Kyle Craig Johnson
+ * ----------------------------------------------------------------------------
+ */
